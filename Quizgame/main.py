0ed65e460f32721f4535986de327a34db459e9bf
ManyaGhand <manyaghand@gmail.com>
2025-07-19 17:53:57 +0530

    Update main.py

--- Quizgame/main.py
+++ Quizgame/main.py
@@ -9,17 +9,10 @@ for question in question_data:  #looping through the text in question data
     question_text=question["text"] #storing the text into "question_text" variable
     question_answer=question["answer"]
     new_question=Question(q_text=question_text,q_answer=question_answer)
-    #storing the text and answer into new question variable
     Question_bank.append(new_question) #adding the new question to question bank list
 
 
 quiz= QuizBrain(Question_bank) #calling the constructor
-#This whole thing is also called naming the object
-#quiz will contain all the data and functions called methods
-#quiz-->object name
-#quizbrain=()-->calling the class to create an object
-#question_bank is the list we created and has been appended with the text and answer from question data
-
 quiz.next_question()
 #the object quiz created from class and accesses the next_question block from QuizBrain
 
