92d03b88838755a237442458a116f707b9a0fd7f
ManyaGhand <manyaghand@gmail.com>
2025-07-19 17:13:15 +0530

    Update question_model.py

--- Quizgame/question_model.py
+++ Quizgame/question_model.py
@@ -1,5 +1,5 @@
 class Question:
     #constructor
-    def __init__(self,q_text,q_answer):  #q_text=parameters="text","answer" from data and "answer"
+    def __init__(self,q_text,q_answer):  
         self.text=q_text
         self.answer=q_answer
