22608e2a1a9ea5552f8ee6b7696ca09d6f4c293d
ManyaGhand <manyaghand@gmail.com>
2025-08-10 20:16:49 +0530

    Add files via upload
    This is a simple Quote Sender program made using SMTP library and Datetime module. 
    It sends a quote from one email to another with the help smtplib and random module.
    If the day of the week is Monday, the Quote sender will send a random quote from quotes.txt file.

---
+++ SMTP_Library/main.py
@@ -0,0 +1,28 @@
+import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+with open("quotes.txt") as quotes:
+    quote = quotes.readlines()
+
+random_quote = random.choice(quote)
+
+email = "manya79997@gmail.com"
+password = "zjcr fkyj vbbs hixw"
+
+if weekday == 6:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls() #blocks any intervention
+        connection.login(user = email , password = password)
+        connection.sendmail(
+            from_addr = email ,
+            to_addrs = "manya79997@yahoo.com",
+                msg = f"Subject: Quote \n\n "
+                      f"{random_quote}."
+        )
+
+
+
